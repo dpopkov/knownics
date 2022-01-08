42478afa3a881c2404a2702cabfcff4f1203d4a0
Denis Popkov <pkvdenis@gmail.com>
2022-01-08 17:29:48 +0300

    Add tests to CategoryServiceImplTest.

--- services/src/test/java/ru/dpopkov/knownics/services/CategoryServiceImplTest.java
+++ services/src/test/java/ru/dpopkov/knownics/services/CategoryServiceImplTest.java
@@ -10,7 +10,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.dpopkov.knownics.domain.question.Category;
 import ru.dpopkov.knownics.domain.question.CategoryRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
@@ -34,6 +39,25 @@ class CategoryServiceImplTest {
     }
 
     @Test
+    void testSaveAll() {
+        service.saveAll(new ArrayList<>());
+        then(repository).should().saveAll(any());
+    }
+
+    @Test
+    void testFindAll() {
+        List<Category> categories = List.of(new Category(), new Category());
+        given(repository.findAll()).willReturn(categories);
+
+        final Iterable<Category> all = service.findAll();
+
+        then(repository).should().findAll();
+        assertTrue(all.iterator().hasNext());
+        all.iterator().next();
+        assertTrue(all.iterator().hasNext());
+    }
+
+    @Test
     void testFindById() {
         service.findById(12L);
         then(repository).should().findById(12L);
@@ -43,5 +67,24 @@ class CategoryServiceImplTest {
     void testDeleteId() {
         service.deleteById(12L);
         then(repository).should().deleteById(12L);
+    }
+
+    @Test
+    void testDelete() {
+        final Category category = new Category();
+        service.delete(category);
+        then(repository).should().delete(category);
+    }
+
+    @Test
+    void testDeleteAll() {
+        service.deleteAll();
+        then(repository).should().deleteAll();
+    }
+
+    @Test
+    void testCount() {
+        service.count();
+        then(repository).should().count();
     }
 }
