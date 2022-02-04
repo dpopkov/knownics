e3189fa106b8c5d823110e3e5949e1d9c3e1885a
Denis Popkov <pkvdenis@gmail.com>
2022-02-04 21:13:04 +0300

    Show single Question with properties.

--- web/src/main/java/ru/dpopkov/knownics/controllers/QuestionController.java
+++ web/src/main/java/ru/dpopkov/knownics/controllers/QuestionController.java
@@ -3,7 +3,9 @@ package ru.dpopkov.knownics.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.dpopkov.knownics.dto.QuestionDto;
 import ru.dpopkov.knownics.services.QuestionService;
 
 @Controller
@@ -20,5 +22,12 @@ public class QuestionController {
     public String list(Model model) {
         model.addAttribute("questions", questionService.findAll());
         return "questions/list";
+    }
+
+    @GetMapping("/{questionId}/show")
+    public String show(@PathVariable Long questionId, Model model) {
+        QuestionDto questionDto = questionService.findById(questionId).orElseThrow();
+        model.addAttribute("question", questionDto);
+        return "questions/show";
     }
 }
--- web/src/test/java/ru/dpopkov/knownics/controllers/QuestionControllerTest.java
+++ web/src/test/java/ru/dpopkov/knownics/controllers/QuestionControllerTest.java
@@ -8,8 +8,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.dpopkov.knownics.dto.QuestionDto;
 import ru.dpopkov.knownics.services.QuestionService;
 
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -23,6 +27,8 @@ class QuestionControllerTest {
 
     MockMvc mockMvc;
 
+    private static final Long QUESTION_ID = 12L;
+
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
@@ -35,5 +41,18 @@ class QuestionControllerTest {
                 .andExpect(view().name("questions/list"))
                 .andExpect(status().isOk());
         then(questionService).should().findAll();
+    }
+
+    @Test
+    void testShow() throws Exception {
+        // Given
+        given(questionService.findById(QUESTION_ID)).willReturn(Optional.of(new QuestionDto()));
+        // When
+        mockMvc.perform(get("/question/{questionId}/show", QUESTION_ID))
+                .andExpect(model().attributeExists("question"))
+                .andExpect(view().name("questions/show"))
+                .andExpect(status().isOk());
+        // Then
+        then(questionService).should().findById(QUESTION_ID);
     }
 }
